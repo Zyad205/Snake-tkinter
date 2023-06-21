62465566a82700cfbe14304df9cc3eb47e2753d7
Zyad Islam <zyadislam201@gmail.com>
2023-06-21 16:59:45 +0300

    Apple class rework and player collision with it's body
    The apple class has changed with same functionality and the player has
    collision system with it's own body and the game has a losing system
    (still under development)

--- Snake.py
+++ Snake.py
@@ -8,43 +8,43 @@ FOOD_COLOR = "#d11404"
 
 
 class Apple:
-    def __init__(self, game):
-        self.game = game
-        self.body = ctk.CTkLabel(self.game, bg_color=FOOD_COLOR, text="")
+    def __init__(self, player_body_pos, window):
+        self.player_body_pos = player_body_pos
+        self.body = ctk.CTkLabel(window, bg_color=FOOD_COLOR, text="")
         self.randomize()
+        self.draw()
 
     def randomize(self):
 
         temp_pos = [randint(0, 29), randint(0, 29)]
         collision = False
 
-        for player_body_part in self.game.player.body_pos:
+        for player_body_part in self.player_body_pos:
             if player_body_part == temp_pos:
                 collision = True
                 break
 
         if collision:
             self.randomize()
+            self.draw()
         else:
             self.position = temp_pos
 
     def draw(self):
         self.body.grid(row=self.position[0], column=self.position[1], sticky="nsew")
 
-    def collision(self):
+    def collision(self, player_body_pos):
         collision = False
 
-        for player_body_part in self.game.player.body_pos:
+        for player_body_part in player_body_pos:
             if player_body_part == self.position:
                 collision = True
                 break
         if collision:
-            self.game.player.score += 1
-            self.game.score_label.configure(text=f"Score: {self.game.player.score}")
-            
             self.randomize()
-            
-            self.game.player.add_block = True
+            self.draw()
+        return collision
+    
 
  
 class Player:
@@ -61,8 +61,12 @@ class Player:
 
         self.score = 0
 
-
         self.body_pos = [[row, column], [row, column - 1]]
+
+    def body_collision(self):
+        for body_part in self.body_pos[1:]:
+            if self.body_pos[0] == body_part:
+                return True
 
     def move_player(self, dir):
         direction_dict = {"w": (-1, 0),
@@ -107,10 +111,12 @@ class Game(ctk.CTk):
 
         self.player = Player()
 
-        self.apple = Apple(self)
+        self.apple = Apple(self.player.body_pos, self)
 
         self.labels = [ctk.CTkLabel(self, bg_color=SNAKE_HEAD_COLOR, text=""),
                        ctk.CTkLabel(self, bg_color=SNAKE_BODY_COLOR, text="")]
+
+        self.game_running = True
 
         self.last_key_pressed = "d"
 
@@ -145,12 +151,36 @@ class Game(ctk.CTk):
                 self.last_key_pressed = event.char
 
     def move(self):
-        self.last_move_dir = self.last_key_pressed
-        self.player.move_player(self.last_key_pressed)
+        if self.game_running:
+            self.last_move_dir = self.last_key_pressed
+            self.player.move_player(self.last_key_pressed)
 
-        self.apple.collision()
-        self.draw()
-        self.after(200, self.move)
+            player_hit_himself = self.player.body_collision()
+            if player_hit_himself:
+                self.game_running = False
+
+            self.apple.player_body_pos = self.player.body_pos
+
+            collision_happened = self.apple.collision(self.player.body_pos)
+
+            if collision_happened:
+                self.player.score += 1
+                self.score_label.configure(text=f"Score: {self.player.score}")
+                self.player.add_block = True
+
+            self.draw()
+            self.after(200, self.move)
+        else:
+            for label in self.labels:
+                label.grid_forget()
+            self.score_label.place_forget()
+            self.apple.body.grid_forget()
+            ctk.CTkLabel(
+                self,
+                text="Game over",
+                font=("Ebrima", 40),
+                text_color="red").grid(row=13, column=13, columnspan=4, rowspan=2)
+
 
     def draw(self):
         if self.player.length != len(self.labels):
@@ -160,7 +190,7 @@ class Game(ctk.CTk):
         for index, label in enumerate(self.labels):
             pos = self.player.body_pos[index]
             label.grid(row=pos[0], column=pos[1], sticky="nsew")
-        self.apple.draw()
+        
 
 
 app = Game()
